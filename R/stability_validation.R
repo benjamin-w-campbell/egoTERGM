69c04542983d89ff833ed665fc6a86c30baed365
Benjamin W. Campbell <bencam92@gmail.com>
2018-09-04 17:32:47 -0400

    Updates.

--- R/stability_validation.R
+++ R/stability_validation.R
@@ -73,7 +73,7 @@
 #'                           tol = 1e-06)
 #'
 #' stability_check <- stability_validation(ego_tergm_fit = ego_tergm_fit)
-#' print(tability_check$)
+#' print(tability_check$comparison_table)
 #' }
 #' @export
 #'
